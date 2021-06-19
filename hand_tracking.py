1000f70067578172d55c4281388bd7e6bdfec807
Edison Cai <edisoncai2001@gmail.com>
2021-06-19 14:28:27 +0100

    detecting hands, drawing landmarks and connections, displaying fps

--- hand_tracking.py
+++ hand_tracking.py
@@ -5,12 +5,18 @@ import time
 # Setting up the webcam
 cap = cv2.VideoCapture(0)
 
+mp_hands = mp.solutions.hands
 # Create a "hands" object
-mpHands = mp.solutions.hands
 # Set things to default values. This means static_mage_mode becomes false
 # This means that we don't have to keep constantly detecting the hands, decreasing performance
 # As long as the tracking confidence is high, no detection is needed
-hands = mpHands.Hands()
+hands = mp_hands.Hands()
+
+# Getting drawing utilities for easy hand drawing
+mp_draw = mp.solutions.drawing_utils
+
+prev_time = 0
+current_time = 0
 
 # Continually show webcam frames
 while True:
@@ -19,5 +25,21 @@ while True:
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(0)
+    # print(results.multi_hand_landmarks)
+
+    # If several hands are detected, loop through them
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            # Drawing landmarks and connections on img
+            mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+    # Calculating the FPS
+    current_time = time.time()
+    fps = 1 / (current_time - prev_time)
+    prev_time = current_time
+
+    # Drawing the FPS onto img
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 0, 255), 3)
+
+    cv2.imshow("Image", cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE))
+    cv2.waitKey(1)
